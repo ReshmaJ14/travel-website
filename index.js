8538841b63fe1e483c66526472b0aa81bb78adbb
Reshma Janvalkar <reshmajanvalkar@gmail.com>
2024-05-24 14:49:38 +0530

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+let group={
+    courseName:"Web development",
+    price:3000,
+    mainGoals:['learn html','learn css ','learn JS']
+}
+alert(group.mainGoals[1])
+
+function goalName(){
+    mainGoals="new";
+}
+goalName()
+alert(mainGoals);
